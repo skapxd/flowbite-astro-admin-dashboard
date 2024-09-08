e41d49a34b3726beef5b004e821a74da3e059471
Manuel <hbiaser132@gmail.com>
2024-09-08 09:56:56 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
-  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  site: process.env.CI ? 'https://flowbite-astro-admin-dashboard-rho.vercel.app' : `http://localhost:${DEV_PORT}`,
+  // base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   /* Like Vercel, Netlify,… Mimicking for dev. server */
   // trailingSlash: 'always',
 
