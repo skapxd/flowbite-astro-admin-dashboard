583cb89cf9a51eb6de522696b82f5be2d421253b
Manuel <hbiaser132@gmail.com>
2024-09-08 09:11:30 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from "@astrojs/vercel/serverless";
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
+	site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
 
-	// output: 'server',
-
+	output: 'hybrid',
+	adapter: vercel(),
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
 	// trailingSlash: 'always',
 
 	server: {
 		/* Dev. server only */
-		port: DEV_PORT,
+		port: DEV_PORT
 	},
-
-	integrations: [
-		//
-		sitemap(),
-		tailwind(),
-	],
+	integrations: [sitemap(), tailwind()],
 });
